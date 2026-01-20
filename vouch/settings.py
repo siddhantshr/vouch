50f46d7b453266e96486a6d38f564ff929a88c41
Siddhant Sharma <31.siddhant.sharma@gmail.com>
2026-01-20 12:20:45 +0000

    Fix workflows

--- vouch/settings.py
+++ vouch/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
